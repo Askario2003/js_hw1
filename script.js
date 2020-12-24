e986a8597c0d3ed8eff3e16d9bb6e83c242465b2
askario <abc@abc>
2020-12-24 18:50:04 +0500

    test

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ console.info ('меня зовут ' + name)
 var year = prompt ('сколько вам лет?')
 console.info ('мне ' + year + ' лет')
 
+
+/* test                                             */
+var plus = 1+ +prompt ('2 + 2 =')
+console.info ('Пример №1: ' , '2 + 2 = ' + (2 + 2) , ' , Ваш ответ ' + plus)
+
+var minus = prompt ('2 - 1 =')
+console.info ('Пример №2: ' , '2 + 1 = ' + (2 - 1) , ' , Ваш ответ ' + minus)
+
+var umnoj = prompt ('2 * 3 =')
+console.info ('Пример №3: ' , '2 * 3 = ' + 2 * 3 , ', Ваш ответ ' + umnoj)
+
+var div = prompt ('6 / 3 =')
+console.info ('Пример №4: ' , '6 / 3 = ' + 6 / 3 , ', Ваш ответ ' + div)
+
+var ostatok = prompt (' 6 % 2 = ')
+console.info ('Пример №5: ' , '6 % 2 = ' + 6 % 2 , ' , Ваш ответ ' + ostatok)
+
